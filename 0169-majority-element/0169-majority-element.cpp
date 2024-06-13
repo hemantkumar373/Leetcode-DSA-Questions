cc5ad6511c42c58dab30702dfb8888234ef7262f
Hemant Kumar <123319697+hemantkumar373@users.noreply.github.com>
2024-06-13 20:58:32 +0530

    Time: 18 ms (53.72%), Space: 26.6 MB (90.64%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int,int>mpp;
+        int count=0;
+        int el;
+        for(int i=0;i<nums.size();i++){
+            if(count==0){
+                count=1;
+                el=nums[i];
+            }
+            else if(el==nums[i]){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return el;
+    }
+};
