477ecf5652d28495c991b5a73785d6573c52718b
Hemant Kumar <123319697+hemantkumar373@users.noreply.github.com>
2024-10-04 22:25:16 +0530

    Time: 19 ms (44.63%), Space: 19.2 MB (24.82%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -2,17 +2,12 @@ class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
         int N = numCourses;
+        vector<int> inDegree(N, 0);
         vector<int> adj[N];
+        
         for(auto it : prerequisites){
             adj[it[0]].push_back(it[1]);
-        }
-        
-        int inDegree[N];
-        for(int i=0; i<N; i++) inDegree[i] = 0;
-        for(int i=0; i<N; i++){
-            for(auto it : adj[i]){
-                inDegree[it]++;
-            }
+            inDegree[it[1]]++;
         }
         
         queue<int> q;
@@ -20,17 +15,17 @@ public:
             if(inDegree[i] == 0) q.push(i);
         }
         
-        int cnt = 0;
+        vector<int> ans;
         while(!q.empty()){
             int node = q.front();
             q.pop();
-            cnt++;
+            ans.push_back(node);
             
             for(auto it : adj[node]){
                 inDegree[it]--;
                 if(inDegree[it] == 0) q.push(it);
             }
         }
-        return cnt == N;
+        return ans.size() == N;
     }
 };
