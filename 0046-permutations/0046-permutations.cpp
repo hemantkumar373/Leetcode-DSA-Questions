e7d9bfce3e830dd09ec6e87cc08037e06890905a
Hemant Kumar <123319697+hemantkumar373@users.noreply.github.com>
2025-01-28 08:30:21 +0530

    Time: 0 ms (100%), Space: 10.5 MB (70.2%) - LeetHub

---
+++ 0046-permutations/0046-permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void findPermute(int idx, vector<int>& nums, vector<vector<int>>& ans) {
+        if (idx == nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i = idx; i < nums.size(); i++) {
+            swap(nums[idx], nums[i]);
+            findPermute(idx + 1, nums, ans);
+            swap(nums[idx], nums[i]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        findPermute(0, nums, ans);
+        return ans;
+    }
+};
